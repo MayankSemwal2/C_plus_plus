2824b84177f760b310394697177a84d9cbfb9368
MasterCodeur <91174448+MasterCodeur@users.noreply.github.com>
2021-09-29 10:39:10 +0530

    Create task_2_2_namespace.cpp

---
+++ practical 4/task_2_2_namespace.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+namespace first{
+int add(int a,int b)
+{
+return (a+b);
+}
+}
+namespace second{
+float add(float a,float b)
+{
+return (a+b);
+}
+}
+using namespace std;
+using namespace first;
+//using namespace second;
+int main()
+{
+cout<<add(2.1,56.0)<<"\n";
+cout<<add(2.1,56)<<"\n";
+cout<<add(2,56)<<"\n";
+cout<<add(2.1,56.0)<<"\n";
+cout<<add(2.1,56)<<"\n";
+cout<<add(2,56)<<"\n";
+
+
+
+
+return 0;
+}
