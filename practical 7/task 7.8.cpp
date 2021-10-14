fd3d39539c93d7d505a803a5bce560ca1008f6cb
MasterCodeur <91174448+MasterCodeur@users.noreply.github.com>
2021-10-14 10:27:36 +0530

    Create task 7.8.cpp

---
+++ practical 7/task 7.8.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+char a[4][4];
+for(int i=0;i<4;i++)
+{
+for(int j=0;j<4;j++)
+{
+a[i][j]='*';
+}}
+for(int i=0;i<4;i++)
+{
+for(int j=0;j<4;j++)
+{
+if(i+j>=3)
+cout<<a[i][j];
+else
+cout<<" ";
+}
+cout<<"\n";
+}
+return 0;
+}
