279e3100cf58356cc83b348ec2ec04fbf2f0568a
MasterCodeur <91174448+MasterCodeur@users.noreply.github.com>
2021-12-13 13:43:07 +0530

    Create task4.cpp

---
+++ practical 11/task4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+//super parent class
+class A{
+public:
+    void name(){
+         cout << "This is class A \n";
+    }
+};
+
+//base class I
+class B: public A{};
+
+//base class II 
+class C: public A{};
+ 
+//derived class
+class D: public B, public C{};
+ 
+int main()
+{
+    D d;
+    d.name();
+ 
+    return 0;
+}
